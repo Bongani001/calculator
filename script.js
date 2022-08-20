c168c5b5da3515d17858bd8e0b96c855707b6ac9
Bongani001 <bbongs421@gmail.com>
2022-08-20 14:59:03 +0200

    add files

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function operate(opr, a, b) {
+    switch (opr) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            if (b == 0) {
+                return 'ERROR! cannot divide by 0';
+            };
+            return divide(a, b);
+    }
+};
+
